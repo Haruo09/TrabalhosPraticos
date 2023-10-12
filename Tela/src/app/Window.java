315d557f86933b21d2883238dd14ba999367300a
Fábio Haruo de Freitas Nishioka <fabio.hn01@gmail.com>
2023-10-12 09:51:59 -0300

    12-10-2023

--- Tela/src/app/Window.java
+++ Tela/src/app/Window.java
@@ -2,6 +2,8 @@ package app;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Window extends JFrame {
     // Definindo os Labels:
@@ -29,8 +31,18 @@ public class Window extends JFrame {
     JRadioButton rbtnSerie2 = new JRadioButton("2°");
     JRadioButton rbtnSerie3 = new JRadioButton("3°");
 
+    // Criando um novo Button Group:
+    ButtonGroup btnGroup = new ButtonGroup();
+
     // Definindo o Slider:
     JSlider sldSatisfacao = new JSlider(JSlider.HORIZONTAL, 0, 100, 1);
+
+    // Definindo os labels finais:
+    JLabel lblInputedName = new JLabel();
+    JLabel lblInputedDate = new JLabel();
+    JLabel lblInputedCurse = new JLabel();
+    JLabel lblInputedSerie = new JLabel();
+    JLabel lblInputedSatisfaction = new JLabel();
 
     // Construtor:
     public Window(String title) throws HeadlessException {
@@ -60,7 +72,6 @@ public class Window extends JFrame {
         txtNome.setBounds(10, 30, 300, 20);
         txtDataNasc.setBounds(320, 30, 150, 20);
 
-
         // Adicionando os elementos à janela:
         this.add(txtNome);
         this.add(txtDataNasc);
@@ -75,19 +86,33 @@ public class Window extends JFrame {
         lblSerie.setBounds(170, 50, 150, 20);
         lblSatisfacao.setBounds(320, 50, 100, 20);
 
-        // Adicionando os elementos à Janela:
+        // Posicionando os Labels de input:
+        lblInputedName.setBounds(10, 120, 300, 20);
+        lblInputedDate.setBounds(10, 150, 300, 20);
+        lblInputedCurse.setBounds(10, 180, 300, 20);
+        lblInputedSerie.setBounds(10, 210, 300, 20);
+        lblInputedSatisfaction.setBounds(10, 240, 300, 20);
+
+        // Adicionando os Labels à Janela:
         this.add(lblNome);
         this.add(lblDataNasc);
         this.add(lblCurso);
         this.add(lblSerie);
         this.add(lblSatisfacao);
+
+        // Adicionando os outros labels à janela:
+        this.add(lblInputedName);
+        this.add(lblInputedDate);
+        this.add(lblInputedCurse);
+        this.add(lblInputedSerie);
+        this.add(lblInputedSatisfaction);
     }
 
     public void addComboBoxes() {
         // Posicionando a ComboBox:
         cmbCurso.setBounds(10, 70, 150, 20);
 
-        // Definindo seu valor padrão:
+        // Definindo valores padrão:
         cmbCurso.setSelectedItem(null);
 
         // Adicionando a ComboBox à janela:
@@ -95,9 +120,6 @@ public class Window extends JFrame {
     }
 
     public void addButtons() {
-        // Criando um novo Button Group:
-        ButtonGroup btnGroup = new ButtonGroup();
-
         // Adicionando os botões ao grupo:
         btnGroup.add(rbtnSerie1);
         btnGroup.add(rbtnSerie2);
@@ -119,19 +141,66 @@ public class Window extends JFrame {
         // Posicionando o Botão:
         btnEnviar.setBounds(368, 100, 100, 20);
 
+        // Programando o botão:
+        btnEnviar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Adquirindo os valores dos campos de entrada:
+                String nome = txtNome.getText();
+                String dataNasc = txtDataNasc.getText();
+                String curso = (String) cmbCurso.getSelectedItem();
+                int serie = (rbtnSerie1.isSelected())?1:
+                    (rbtnSerie2.isSelected())?2:
+                    (rbtnSerie3.isSelected())?3:0;
+                int satisfacao = sldSatisfacao.getValue();
+
+                // Imprimindo os resultados na tela:a
+                if (nome.equals("")) {
+                    lblInputedName.setText("Erro: nome não informado.");
+                    lblInputedName.setForeground(Color.decode("#940b01"));
+                } else {
+                    lblInputedName.setText("Nome: " + nome);
+                    lblInputedName.setForeground(Color.black);
+                }
+
+                if (dataNasc.equals("")) {
+                    lblInputedDate.setText("Erro: data de nascimento não informada.");
+                    lblInputedDate.setForeground(Color.decode("#940b01"));
+                } else {
+                    lblInputedDate.setText("Data de nascimento: " + dataNasc);
+                    lblInputedDate.setForeground(Color.black);
+                }
+
+                if (curso == null) {
+                    lblInputedCurse.setText("Erro: curso não selecionado.");
+                    lblInputedCurse.setForeground(Color.decode("#940b01"));
+                } else {
+                    lblInputedCurse.setText("Curso: " + curso);
+                    lblInputedCurse.setForeground(Color.black);
+                }
+
+                if (serie == 0) {
+                    lblInputedSerie.setText("Erro: Série não selecionada.");
+                    lblInputedSerie.setForeground(Color.decode("#940b01"));
+                } else {
+                    lblInputedSerie.setText("Série: " + Integer.toString(serie) + "º ano");
+                    lblInputedSerie.setForeground(Color.black);
+                }
+
+                lblInputedSatisfaction.setText("Satisfação: " + Integer.toString(satisfacao));
+
+            }
+        });
+
         // Adicionando o Botão:
         this.add(btnEnviar);
     }
 
     public void addSlider() {
+        // Posicionando o Slider:
         sldSatisfacao.setBounds(315, 70, 150, 20);
-        this.add(sldSatisfacao);
-    }
 
-    public void addResults() {
-        // Adquirindo os valores dos campos de entrada:
-        String nome = txtNome.getText();
-        String dataNasc = txtNome.getText();
-        String curso = (String) cmbCurso.getSelectedItem();
+        // Adicionando o Slider:
+        this.add(sldSatisfacao);
     }
 }
--- Tela/src/main/Main.java
+++ Tela/src/main/Main.java
@@ -1,4 +1,6 @@
-package app;
+package main;
+
+import app.Window;
 
 public class Main {
     public static void main(String[] args) {
