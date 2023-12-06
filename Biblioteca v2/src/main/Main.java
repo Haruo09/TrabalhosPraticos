a85cd2353af46149587bc5bf7c7dcbe38c5735b0
Fábio Haruo de Freitas Nishioka <fabio.hn01@gmail.com>
2023-12-06 20:54:28 -0300

    main

---
+++ Biblioteca v2/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import app.frontend.windows.MainPanel;
+
+public class Main {
+    public static void main(String[] args) {
+        MainPanel mainPanel = new MainPanel();
+        mainPanel.setVisible(true);
+    }
+}
